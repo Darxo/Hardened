53034237f37363b100782ef036320d07535f1be1
Darxo <Darxo@users.noreply.github.com>
2026-02-03 06:02:57 +0100

    feat: display terrain type and day/night status in combat dialog

--- ui/mods/mod_hardened/js_hooks/ui/screens/world/world_combat_dialog.js
+++ ui/mods/mod_hardened/js_hooks/ui/screens/world/world_combat_dialog.js
@@ -1,14 +1,64 @@
+var terrainNameTable = {
+	Needles: "Needle Forest",
+	"Forest Snow": "Snow Forest",
+}
+
+var formatTerrainName = function(image) {
+	// Remove everything up to and including "engage_"
+	var name = image.replace(/^.*engage_/, "");
+
+	// Remove ".png" at the end
+	name = name.replace(/\.png$/, "");
+
+	// Detect and remove "_night"
+	var isNight = false;
+	if (name.indexOf("_night") !== -1)
+	{
+		isNight = true;
+		name = name.replace(/_night$/, "");
+	}
+
+	// Replace underscores with spaces
+	name = name.replace(/_/g, " ");
+
+	// Capitalize every word
+	name = name.replace(/\b\w/g, function(c)
+	{
+		return c.toUpperCase();
+	});
+
+	name = terrainNameTable[name] || name;
+
+	if (isNight)
+	{
+		name += " (Night)";
+	}
+	else
+	{
+		name += " (Day)";
+	}
+
+	return name;
+}
+
 Hardened.Hooks.WorldCombatDialog_loadFromData = WorldCombatDialog.prototype.loadFromData;
 WorldCombatDialog.prototype.loadFromData = function ( _data )
 {
+	var oldCreateDialog = $.fn.createDialog;
+	$.fn.createDialog = function(_title, _subTitle, _headerImagePath, _withTabs, _classes)
+	{
+		_subTitle = formatTerrainName(_data.Image);
+		return oldCreateDialog.apply(this, arguments);
+	}
+
 	Hardened.Hooks.WorldCombatDialog_loadFromData.call(this, _data);
 
-	var entities = _data.Entities;
+	$.fn.createDialog = oldCreateDialog;
 
 	// Feat: we improve the vanilla entity display by
 	//	- wrapping it inside a scroll container allowing much more entities to be displayed at once
 	//	- cut off long entity names with "..."
-
+	var entities = _data.Entities;
 	if(entities.length >= 0)
 	{
 		var rightColumn = this.mContentContainer.find('.right-column');
